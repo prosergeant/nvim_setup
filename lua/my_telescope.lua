f31fd55a59c32ed22863a0000d36535793168b95
prosergeant <sberendeyev@gmail.com>
2026-04-07 18:00:37 +0500

    add delete method for branches

--- lua/my_telescope.lua
+++ lua/my_telescope.lua
@@ -1,23 +1,35 @@
--- local actions = require("telescope.actions")
 local action_state = require("telescope.actions.state")
 
 local M = {}
 
--- Выносим функцию удаления отдельно
-M.delete_buf = function(prompt_bufnr)
+-- Универсальная функция удаления
+M.smart_delete = function(prompt_bufnr)
     local current_picker = action_state.get_current_picker(prompt_bufnr)
+    local picker_name = current_picker.prompt_title:lower() -- Узнаем, где мы находимся
+
     current_picker:delete_selection(function(selection)
-        vim.api.nvim_buf_delete(selection.bufnr, { force = true })
+        -- 1. Если мы в БУФЕРАХ
+        if picker_name:find("buffers") then
+            if selection.bufnr and vim.api.nvim_buf_is_valid(selection.bufnr) then
+                vim.api.nvim_buf_delete(selection.bufnr, { force = true })
+            end
+
+        -- 2. Если мы в ВЕТКАХ ГИТА
+        elseif picker_name:find("git branches") then
+            local branch = selection.value
+            -- Удаляем ветку через системную команду
+            vim.fn.system("git branch -D " .. branch)
+            print("Ветка " .. branch .. " удалена")
+        end
     end)
 end
 
--- Настройка самого Telescope
 M.setup = function()
     require('telescope').setup{
       defaults = {
         mappings = {
-          i = { ["<C-d>"] = M.delete_buf },
-          n = { ["dd"] = M.delete_buf },
+          i = { ["<C-d>"] = M.smart_delete },
+          n = { ["dd"] = M.smart_delete },
         },
       },
     }
