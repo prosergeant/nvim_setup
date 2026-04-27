fd7e3c895214e1feba3d21cc275fcf9b497e07bb
Сергей Берендеев <sergeyb0707@kmf.local>
2026-04-27 17:54:06 +0500

    feat(test): add new plugin for tests

--- init.lua
+++ init.lua
@@ -258,6 +258,35 @@ require("lazy").setup({
   --     port = 21036,
   --   })
   -- end},
+  --
+
+  {
+    "nvim-neotest/neotest",
+    dependencies = {
+      "nvim-neotest/nvim-nio",
+      "nvim-lua/plenary.nvim",
+      "antoinemadec/FixCursorHold.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      "marilari88/neotest-vitest", -- Адаптер именно для Vitest
+    },
+
+    config = function()
+      require("neotest").setup({
+        adapters = {
+          require("neotest-vitest"),
+        },
+      })
+
+      -- Быстрые клавиши
+      vim.keymap.set("n", "<leader>tr", function() require("neotest").run.run() end,
+        { desc = "Запустить ближайший тест" })
+      vim.keymap.set("n", "<leader>ts", function() require("neotest").summary.toggle() end,
+        { desc = "Показать панель тестов" })
+      vim.keymap.set("n", "<leader>to", function() require("neotest").output.open({ enter = true }) end,
+        { desc = "Показать вывод теста" })
+    end,
+  }
+
 })
 
 -- Vue: динамический commentstring по секции (template/script/style)
@@ -400,7 +429,7 @@ end, { desc = "Git checkout file from branch" })
 map("n", "<leader>tn", ":tabnew<CR>", { desc = "New tab" })
 map("n", "<leader>tc", ":tabclose<CR>", { desc = "Close tab" })
 
--- Добавить строку не переходя в режим insert 
+-- Добавить строку не переходя в режим insert
 map("n", "]<Space>", "o<Esc>", { desc = "New line below" })
 map("n", "[<Space>", "O<Esc>", { desc = "New line above" })
 
